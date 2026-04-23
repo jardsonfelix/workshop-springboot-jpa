9b9f8e9d11b54dbbf79adb0452e72120d5faba1b
Jardson Felix <jardsonjfs32@gmail.com>
2026-04-22 23:04:34 -0300

    classes User e UserResource

---
+++ src/main/java/com/projetosspring/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projetosspring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetosspring.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "maria@123.com", "9999999", "123456" );
+		return ResponseEntity.ok().body(u);
+	}
+
+}
